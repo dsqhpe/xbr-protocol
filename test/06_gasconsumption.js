1a49d3998aebb29380325d7f1a7e5b57460b6015
albert <albert.kos@continental-corporation.com>
2020-03-09 18:04:43 +0100

    DM10-37: token transfer costs
    minimal example on how to get transaction costs from within truffle test with ethgasstation for gasprice definition, get gasprice only once

--- test/06_gasconsumption.js
+++ test/06_gasconsumption.js
@@ -11,12 +11,11 @@ const web3 = require("web3");
 var XBRToken = artifacts.require("./XBRToken.sol");
 const request = require('request');
 
-//https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethgettransactionreceipt
 contract('XBRToken', function (accounts) {
 
     var gaspriceInEuro = null;
     const Eth2Euro = 167
-    beforeEach('get gasprice from gasstation before test', function(done) {
+    before('get gasprice from gasstation before test', function(done) {
         request('https://ethgasstation.info/json/ethgasAPI.json', { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
             gaspriceInEuro = body.average * 0.0000000001 * Eth2Euro 
