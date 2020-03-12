deff3fecc8a1c72de8724fd12fef144e80613a45
albert <albert.kos@continental-corporation.com>
2020-03-12 16:28:32 +0100

    DM10-37: XBR Cost Overview
    Cost overview for all Actors buyer/seller/operator see README.md output of test for this day

--- test/06_gasconsumption.js
+++ test/06_gasconsumption.js
@@ -17,7 +17,10 @@ const request = require('request');
 contract('XBRToken', function (accounts) {
 
     var gaspriceInEuro = null;
-    const Eth2Euro = 167
+    const Eth2Euro = 167;
+    var market_maker = accounts[2];
+    var buyer = accounts[3];
+
     before('get gasprice from gasstation before test', function(done) {
         request('https://ethgasstation.info/json/ethgasAPI.json', { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
@@ -29,7 +32,8 @@ contract('XBRToken', function (accounts) {
 
     it("XBRToken gas consumption: token transfer", async () => {
         let token = await XBRToken.deployed();
-        let hash = await token.transfer(accounts[5], 9000);
+        let hash = await token.transfer(market_maker, 1001);
+        hash = await token.transfer(buyer, 1002)
         tx_cost = hash.receipt.gasUsed * gaspriceInEuro
         console.log("transaction costs " + tx_cost + " Euro")
         assert(tx_cost < 0.13, "transaction to expensive -> " + tx_cost + " Euro")
@@ -38,11 +42,22 @@ contract('XBRToken', function (accounts) {
 });
 
 contract('XBRNetwork', function(accounts){
-    //global test
-    var network;
-    var token;
+
     var gaspriceInEuro = null;
     const Eth2Euro = 167
+    var contract_owner = accounts[0];
+    var market_operator = accounts[1];
+    var market_maker = accounts[2];
+    var buyer = accounts[3];
+    var buyer_delegate = accounts[4];
+    var seller = accounts[5];
+    var seller_delegate = accounts[6];
+    const marketId = web3.utils.sha3("TestMarket").substring(0, 34);
+
+    var registration_gasConsumptio = 0
+    var createMarket_gasConsumptio = 0
+    var joinMarket_gasConsumptio = 0
+    var buyerChannel_gasConsumptio = 0
 
     before('get gasprice from gasstation before test', function(done) {
         
@@ -54,75 +69,77 @@ contract('XBRNetwork', function(accounts){
         });
     });
 
-    beforeEach('setup contract for each test', async () => {
+    before('setup contract for each test', async () => {
 
-        network = await XBRNetwork.deployed();
-        token = await XBRToken.deployed();
-        market = await XBRMarket.deployed();
+        const network = await XBRNetwork.deployed();
+        const token = await XBRToken.deployed();
+        const market = await XBRMarket.deployed();
 
         console.log('Using XBRNetwork         : ' + network.address);
         console.log('Using XBRToken           : ' + token.address);
-    })
+        console.log('Using XBRMarket          : ' + market.address);
 
-    //you can save 5 eurocents if you are registering without profile
-    it("XBRNetwork gas consumption: register", async () => {
         const eula = await network.eula();
         //we are only using this to have a IPFS hash sized value
         const profile = eula;
-        bob = accounts[1];
-        carl = accounts[2];
-        let hash = await network.register(eula, '', {from: bob});
-        tx_cost = hash.receipt.gasUsed * gaspriceInEuro
-        console.log("register costs without profile string: " + tx_cost + " Euro")
-        assert(tx_cost < 0.16, "registration to expensive -> " + tx_cost + " Euro")
-        hash = await network.register(eula, profile, {from: carl});
-        tx_cost = hash.receipt.gasUsed * gaspriceInEuro
-        console.log("register costs " + tx_cost + " Euro")
-        assert(tx_cost < 0.24, "registration to expensive -> " + tx_cost + " Euro")
-    });
-
-    it("create market gas consumption", async () => {
-        const alice = accounts[0];
-        const maker = accounts[1];
         const terms = "";
         const meta = "";
         const marketFee = 0;
-        const marketId = web3.utils.sha3("TestMarket").substring(0, 34);
         const consumerSecurity = 0;
-        const providerSecurity = 1;
-        let hash = await market.createMarket(marketId, terms, meta, maker, providerSecurity, consumerSecurity, marketFee, {from: alice});
-        tx_cost = hash.receipt.gasUsed * gaspriceInEuro
-        console.log("create market costs " + tx_cost + " Euro")
-        assert(tx_cost < 0.37, "transaction to expensive -> " + tx_cost + " Euro")
-    })
-
-    it("Join Market gas consumption", async () => {
-
-        const providerSecurity = 1000;
-        const owner = accounts[0];
-        const provider = accounts[5];
+        const providerSecurity = 0;
         const ActorTyp_PROVIDER = 1;
-        const marketId = web3.utils.sha3("TestMarket").substring(0, 34);
-        const meta = "";
+        const ActorTyp_CONSUMER = 2;
 
-        //register provider in network
-        const eula = await network.eula();
-        let hash = await network.register(eula, '', {from: provider});
+        //registration without profile cheaper because data not stored in contract 
+        registration_gasConsumptio = await network.register(eula, profile, {from: market_operator});
+        registration_gasConsumptio = await network.register(eula, '', {from: market_maker});
+        registration_gasConsumptio = await network.register(eula, '', {from: seller});
+        registration_gasConsumptio = await network.register(eula, '', {from: buyer});
 
-        // transfer 1000 XBR to provider
-        await token.transfer(provider, 2000000, {from: owner});
+        createMarket_gasConsumptio = await market.createMarket(marketId, terms, meta, market_maker, 
+                                providerSecurity, consumerSecurity, marketFee, {from: market_operator});
 
-        // approve transfer of tokens to join market
-        const txn_approve = await token.approve(provider, providerSecurity, {from: provider});
+        joinMarket_gasConsumptio = await market.joinMarket(marketId, ActorTyp_PROVIDER, meta, {from: seller});
+        joinMarket_gasConsumptio = await market.joinMarket(marketId, ActorTyp_CONSUMER, meta, {from: buyer});
 
-        // var token = await network.token()
-        // var txn = await token.transferFrom(provider, owner, 10, {from: provider});
-        // XBR provider joins market
-        const txn_join = await market.joinMarket(marketId, ActorTyp_PROVIDER, meta, {from: provider});
 
-        tx_cost = (txn_approve.receipt.gasUsed + txn_join.receipt.gasUsed) * gaspriceInEuro
-        console.log("join market costs " + tx_cost + " Euro")
-        assert(tx_cost < 0.37, "join market to expensive -> " + tx_cost + " Euro")
+        await token.transfer(buyer, 100)
+        await token.approve(market.address, 10, {from: buyer});
+        //only market owner is allowed to get off-chain transaction ?? unclear why! 
+        buyerChannel_gasConsumptio = await market.openPaymentChannel(marketId, market_operator, buyer_delegate, 10, 860000, {from: buyer})
+
+        await token.transfer(market_maker, 100)
+        await token.approve(market.address, 10, {from: market_maker});
+        sellerChannel_gasConsumptio = await market.openPayingChannel(marketId, seller, seller_delegate, 10, 666000, {from: market_maker})
     })
 
+    it("Registration costs:", () => {
+        tx_cost = registration_gasConsumptio.receipt.gasUsed * gaspriceInEuro
+        console.log("register costs are: " + tx_cost + " Euro")
+        assert(tx_cost < 0.16, "registration to expensive -> " + tx_cost + " Euro")
+    })
+
+    it("Create Market costs:", () => {
+        tx_cost = createMarket_gasConsumptio.receipt.gasUsed * gaspriceInEuro
+        console.log("create market costs are: " + tx_cost + " Euro")
+        assert(tx_cost < 0.37, "create market to expensive -> " + tx_cost + " Euro")
+    })
+
+    it("Join Market costs:", () => {
+        tx_cost = joinMarket_gasConsumptio.receipt.gasUsed * gaspriceInEuro
+        console.log("join market costs: " + tx_cost + " Euro")
+        assert(tx_cost < 0.38, "join market to expensive -> " + tx_cost + " Euro")
+    })
+
+    it("Open Buyer Channel costs:", () => {
+        tx_cost = buyerChannel_gasConsumptio.receipt.gasUsed * gaspriceInEuro
+        console.log("open market costs: " + tx_cost + " Euro")
+        assert(tx_cost < 0.38, "join market to expensive -> " + tx_cost + " Euro")
+    })
+
+    it("Open Seller Channel costs: ", () => {
+        tx_cost = sellerChannel_gasConsumptio.receipt.gasUsed * gaspriceInEuro
+        console.log("Open Seller Channel costs " + tx_cost + " Euro")
+        assert(tx_cost < 0.38, "Open Seller Channel to expensive -> " + tx_cost + " Euro")
+    })
 })
